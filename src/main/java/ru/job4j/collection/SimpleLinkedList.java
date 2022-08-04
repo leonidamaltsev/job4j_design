59db422d24e7647217b9330c5ec6b65d5230628e
leonidamaltsev <daf.dof@yandex.ru>
2022-08-04 18:42:54 +0300

    2. Создать контейнер на базе связанного списка [#159 #328875]

--- src/main/java/ru/job4j/collection/SimpleLinkedList.java
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -1,51 +1,68 @@
 package ru.job4j.collection;
 
-import java.util.*;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 
 public class SimpleLinkedList<E> implements LinkedList<E> {
 
-    private E[] container;
-
+    private Node<E> first;
+    private Node<E> last;
     private int size;
-
     private int modCount;
+
+    private static class Node<E> {
+        E data;
+        Node<E> next;
+
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
 
     @Override
     public void add(E value) {
-        modCount++;
-        if (size == container.length) {
-            growContainer();
+        Node<E> newList = new Node<>(value, null);
+        newList.data = value;
+        if (isEmpty()) {
+            newList = first;
+            last = newList;
+        } else {
+            last.next = newList;
         }
-        container[size] = value;
+        last = newList;
+        modCount++;
         size++;
     }
 
     @Override
     public E get(int index) {
         Objects.checkIndex(index, size);
-        return container[index];
-    }
-
-    private void growContainer() {
-        if (container.length == 0) {
-            container = Arrays.copyOf(container, 10);
-        } else {
-            container = Arrays.copyOf(container, container.length * 2);
+        Node<E> result = first;
+        for (int i = 0; i < index; i++) {
+            result = first.next;
         }
+        return result.data;
     }
 
     @Override
     public Iterator<E> iterator() {
         return new Iterator<>() {
-            int index = 0;
-            int expectedModCount = modCount;
+            private Node<E> index = first;
+            final int expectedModCount = modCount;
 
             @Override
             public boolean hasNext() {
                 if (expectedModCount != modCount) {
                     throw new ConcurrentModificationException();
                 }
-                return size > index;
+                return index != null;
             }
 
             @Override
@@ -53,7 +70,9 @@ public class SimpleLinkedList<E> implements LinkedList<E> {
                 if (!hasNext()) {
                     throw new NoSuchElementException();
                 }
-                return container[index++];
+                E element = index.data;
+                index = index.next;
+                return element;
             }
         };
     }
