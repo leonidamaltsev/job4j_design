e825461bcca7eac4cb9ba8797c083edc406ff8d0
leonidamaltsev <daf.dof@yandex.ru>
2022-08-22 22:58:16 +0300

    3. Переопределить только hashCode [#1003]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -3,6 +3,7 @@ package ru.job4j.map;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class User {
     private String name;
@@ -22,17 +23,37 @@ public class User {
         int hashCode1 = user1.hashCode();
         int hash1 = hashCode1 ^ (hashCode1 >>> 16);
         int bucket1 = hash1 & 15;
+        int index1 = ((16 - 1) & user1.hashCode());
         User user2 = new User("Alex", 1, birthday);
         int hashCode2 = user2.hashCode();
         int hash2 = hashCode2 ^ (hashCode1 >>> 16);
         int bucket2 = hash2 & 15;
+        int index2 = ((16 - 1) & user2.hashCode());
         map.put(user1, new Object());
         map.put(user2, new Object());
-        System.out.printf("user1 - хэш-код: %s , хэш: %s, backet: %s ",
-                hashCode1, hash1, bucket1);
-        System.out.printf("user2 - хэш-код: %s , хэш: %s, backet: %s",
-                hashCode2, hash2, bucket2);
+        System.out.printf("user1 - хэш-код: %s , хэш: %s, backet: %s , user1 index: %s",
+                hashCode1, hash1, bucket1, index1);
+        System.out.printf("user2 - хэш-код: %s , хэш: %s, backet: %s, user2 index: %s",
+                hashCode2, hash2, bucket2, index2);
         System.out.println(user1.equals(user2));
-
     }
+    /*
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof User)) {
+            return false;
+        }
+        User user = (User) obj;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }*/
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
+
 }
