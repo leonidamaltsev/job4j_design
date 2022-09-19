3927adbb017dcf28f72eaf1688e3c522bd47c6ec
leonidamaltsev <daf.dof@yandex.ru>
2022-09-19 16:40:36 +0300

    0.1. FileOutputStream. [#252488 #344237]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -2,25 +2,17 @@ package ru.job4j.io;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 
 public class ResultFile {
 
-    public static int[][] multiple(int size) {
-        int[][] multiple = new int[size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                multiple[i][j] = (i + 1) * (j + 1);
-            }
-        }
-        return multiple;
-    }
-
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("multiple.txt")) {
-            out.write(Arrays.deepToString(ResultFile.multiple(4)).getBytes());
-            out.write(System.lineSeparator().getBytes());
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    out.write(((i + 1) * (j + 1) + " ").getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
