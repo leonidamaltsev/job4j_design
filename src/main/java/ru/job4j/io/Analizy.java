6db7919868b4007a01ce83215ae73b6c8f81883c
leonidamaltsev <daf.dof@yandex.ru>
2022-09-30 14:46:35 +0300

    2. Анализ доступности сервера. [#859 #347375]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -12,20 +12,18 @@ public class Analizy {
      * Результат анализа записан в файл unavailable.csv
      */
     public void unavailable(String source, String target) {
-        try (BufferedReader read = new BufferedReader(new FileReader(source))) {
-            PrintWriter out = new PrintWriter(new FileOutputStream(target));
+        try (BufferedReader read = new BufferedReader(new FileReader(source));
+            PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
             boolean startWrite = false;
-            for (String line : read.lines().toList()) {
-                boolean isAvailable = line.startsWith("200") || line.startsWith("300");
-                if (!isAvailable && !startWrite) {
-                    out.write(String.format("%s;", line.split(" ")[1]));
-                    startWrite = true;
-                } else if (isAvailable && startWrite) {
-                    out.write(String.format("%s;%n", line.split(" ")[1]));
-                    startWrite = false;
+            while (read.ready()) {
+                String[] s = read.readLine().split(" ", 2);
+                boolean isAvailable = Integer.parseInt(s[0]) < 400;
+                if (!isAvailable == !startWrite) {
+                    startWrite = !startWrite;
+                    out.append(s[1]).append(';')
+                            .append((startWrite ? "" : System.lineSeparator()));
                 }
             }
-            out.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
