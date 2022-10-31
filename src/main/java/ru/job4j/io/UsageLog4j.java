f5b7277767dbf5a98e8cc100b4c2132f4e377ca1
leonidamaltsev <daf.dof@yandex.ru>
2022-10-31 22:29:30 +0300

    1. Log4j. Логирование системы. [#95335 #356133]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -1,7 +1,7 @@
 package ru.job4j.io;
 
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 public class UsageLog4j {
 
