bdcdc48580a1250dd63c43caf7728b5c367a842e
leonidamaltsev <daf.dof@yandex.ru>
2023-01-20 15:53:03 +0300

    0. Select [#6861 #383018]

--- sql/scriptOneSelect.sql
+++ sql/scriptOneSelect.sql
@@ -80,3 +80,30 @@ insert into students(name, course, budget, speciality, enroll_date, university_i
 values ('EJ', 4, true, 'J5', date '2017-09-01', 5);
 insert into students(name, course, budget, speciality, enroll_date, university_id)
 values ('EI', 5, true, 'K5', date '2016-09-01', 5);
+
+select * from students;
+select name, course, speciality from students;
+
+select * from students where course = 2;
+select * from students where course != 2;
+
+select * from students where name is null;
+select * from students where name is not null;
+
+select * from students where enroll_date > '01.09.2020';
+select * from students where course > 2;
+
+select * from students where name like 'D%';
+select * from students where name like 'D%' and course > 2;
+select * from students where name like 'D%' or course > 2;
+
+select current_date;
+select current_date > '10.09.2020';
+select current_date + interval '1 month';
+
+select * from students order by speciality asc;
+select * from students order by speciality desc;
+
+select distinct course from students;
+
+select * from students limit 5;
