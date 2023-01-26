764515e9dabcf294b389ee75069e254ecffbc76d
leonidamaltsev <daf.dof@yandex.ru>
2023-01-26 16:52:43 +0300

    3. Фильтры [#1732 #385059]

--- sql/scriptFilters.sql
+++ sql/scriptFilters.sql
@@ -1,3 +1,9 @@
+create table type
+(
+	id serial primary key,
+	name varchar(255)		
+);
+
 create table product
 (
 	id serial primary key,
@@ -7,28 +13,22 @@ create table product
 	price float
 );
 
-create table type
-(
-	id serial primary key,
-	name varchar(255)		
-);
+insert into product(name, type_id, expired_date, price) values ('Сыр плавленный', 1, '2023-01-25', 250.00);
+insert into product(name, type_id, expired_date, price) values ('Сыр моцарелла', 1, '2023-01-25', 350.00);
+insert into product(name, type_id, expired_date, price) values ('Сыр гройцер', 1, '2024-01-25', 950.00);
 
-insert into product(name, type_id, expired_date, price) values ('Сыр плавленный', 1, date '2023-01-25', 250.00);
-insert into product(name, type_id, expired_date, price) values ('Сыр моцарелла', 1, date '2023-01-25', 350.00);
-insert into product(name, type_id, expired_date, price) values ('Сыр гройцер', 1, date '2024-01-25', 950.00);
+insert into product(name, type_id, expired_date, price) values ('Молоко агуша', 2, '2022-01-25', 155.00);
+insert into product(name, type_id, expired_date, price) values ('Молоко можайское', 2, '2024-01-25', 105.00);
+insert into product(name, type_id, expired_date, price) values ('Молоко ромашка', 2, '2023-01-25', 75.00);
 
-insert into product(name, type_id, expired_date, price) values ('Молоко агуша', 2, date '2022-01-25', 155.00);
-insert into product(name, type_id, expired_date, price) values ('Молоко можайское', 2, date '2024-01-25', 105.00);
-insert into product(name, type_id, expired_date, price) values ('Молоко ромашка', 2, date '2023-01-25', 75.00);
-
-insert into product(name, type_id, expired_date, price) values ('фруктовое мороженное', 3, date '2023-01-25', 55.00);
-insert into product(name, type_id, expired_date, price) values ('мороженное Фисташка', 3, date '2023-01-25', 100.00);
-insert into product(name, type_id, expired_date, price) values ('мороженное Лакомка', 3, date '2024-01-25', 150.00);
+insert into product(name, type_id, expired_date, price) values ('фруктовое мороженное', 3, '2023-01-25', 55.00);
+insert into product(name, type_id, expired_date, price) values ('мороженное Фисташка', 3, '2023-01-25', 100.00);
+insert into product(name, type_id, expired_date, price) values ('мороженное Лакомка', 3, '2024-01-25', 150.00);
 
 
-insert into product(name) values ('Сыр');
-insert into product(name) values ('Молоко');
-insert into product(name) values ('Мороженное');
+insert into type(name) values ('Сыр');
+insert into type(name) values ('Молоко');
+insert into type(name) values ('Мороженное');
 
 select * from product as p
 join type as t
@@ -40,9 +40,9 @@ join type as t
 on p.type_id = t.id
 where t.name = 'мороженное';
 
-select * from product where product.expired_date < '2023-01-02';
+select * from product where expired_date < current_date;
 
-select max(product.price) from product;
+select * from product where price = (select max(price) from product);
 
 select t.name, count(*) 
 from product as p join type as t 
