466c2bf0084c72fcd15c09df7dea5b21e391552c
leonidamaltsev <daf.dof@yandex.ru>
2023-01-30 23:05:26 +0300

    0. Join [#6862 #386273]

--- sql/outer_join.sql
+++ sql/outer_join.sql
@@ -14,21 +14,58 @@ create table employees
 insert into departments(name) values ('Departm1');
 insert into departments(name) values ('Departm2');
 insert into departments(name) values ('Departm3');
+insert into departments(name) values ('Departm4');
+insert into departments(name) values ('Departm5');
 
 insert into employees(name, department_id) values ('Empl1', 1);
 insert into employees(name, department_id) values ('Empl2', 2);
 insert into employees(name, department_id) values ('Empl3', 3);
-insert into employees(name, department_id) values ('Empl4', null);
-insert into employees(name, department_id) values ('Empl5', null);
+insert into employees(name, department_id) values (null, 4);
+insert into employees(name, department_id) values (null, 5);
 insert into employees(name, department_id) values ('Empl6', 1);
 
-select * from employees e left join departments d on e.department_id = d.id;
-select * from employees e left join departments d on e.department_id = d.id where d.id is not null;
-select * from departments d left join employees e on d.id = e.department_id;
-select * from departments d right join employees e on e.department_id = d.id;
-select * from employees e right join departments d on e.department_id = d.id;
-select * from employees e full join departments d on e.department_id = d.id;
-select * from employees e cross join departments d;
+--2. Выполнить запросы с left, right, full, cross соединениями
+select * from employees e 
+left join departments d 
+on e.department_id = d.id;
+
+select * from employees e
+right join departments d
+on e.department_id = d.id;
+
+select * from employees e
+full join departments d
+on e.department_id = d.id;
+
+select * from employees e
+cross join departments d;
+
+--3. Используя left join найти департаменты, у которых нет работников
+select * from departments d
+left join employees e
+on e.department_id = d.id
+where e.name is null;
+
+/* 4. Используя left и right join написать запросы, которые давали бы 
+одинаковый результат (порядок вывода колонок в эти запросах также 
+должен быть идентичный).
+*/
+select * from employees e 
+left join departments d 
+on e.department_id = d.id;
+
+select * from departments d 
+right join employees e 
+on e.department_id = d.id;
+
+select * from departments d 
+left join employees e 
+on d.id = e.department_id;
+
+select * from employees e 
+right join departments d 
+on e.department_id = d.id;
+
 
 create table teens
 (
@@ -43,4 +80,6 @@ insert into teens(name, gender) values ('Evander', 'male');
 insert into teens(name, gender) values ('Roy', 'male');
 insert into teens(name, gender) values ('Tina', 'female');
 
-select * from teens as m cross join teens as f where m.gender = 'male' and f.gender = 'female' 
+select * from teens as m 
+cross join teens as f 
+where m.gender = 'male' and f.gender = 'female' 
