291bf487ed4c932e019297f419ffe3669b983361
leonidamaltsev <daf.dof@yandex.ru>
2023-02-08 16:59:55 +0300

    2. Триггеры [#504803]

---
+++ sql/triggers1.sql
@@ -0,0 +1,33 @@
+create table products (
+    id serial primary key,
+    name varchar(50),
+    producer varchar(50),
+    count integer default 0,
+    price integer,
+	tax integer
+);
+
+insert into products (name, producer, count, price) values ('product_3', 'producer_3', 8, 115);
+insert into products (name, producer, count, price) values ('product_1', 'producer_1', 3, 50);
+	
+create or replace function discount()
+    returns trigger as
+$$
+    BEGIN
+        update products
+        set price = price - price * 0.2
+        where count <= 5 AND id = new.id;
+        return NEW;
+    END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger discount_trigger
+    after insert
+    on products
+    for each row
+    execute procedure discount();
+	
+alter table products disable trigger discount_trigger;
+
+drop trigger discount_trigger on products;
---
+++ sql/triggers2.sql
@@ -0,0 +1,76 @@
+/*
+1)  Триггер должен срабатывать после вставки данных, для любого товара
+и просто насчитывать налог на товар (нужно прибавить налог к цене товара). 
+Действовать он должен не на каждый ряд, а на запрос (statement уровень)
+*/
+
+create or replace function tax()
+    returns trigger as
+$$
+    BEGIN
+        update products
+        set price = price + tax 
+        where id = (select id from inserted);
+        return new;
+    END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger tax_trigger
+    after insert on products
+    referencing new table as inserted
+    for each statement
+    execute procedure tax();
+
+/*
+2) Триггер должен срабатывать до вставки данных и насчитывать 
+налог на товар (нужно прибавить налог к цене товара). 
+Здесь используем row уровень.
+*/
+create or replace function tax_row()
+    returns trigger as
+$$
+    BEGIN
+        update products
+        set price = price + tax
+        where id = new.id;
+        return NEW;
+    END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger tax_row_trigger
+    before insert
+    on products
+    for each row
+    execute procedure tax_row();
+	
+/*
+триггер на row уровне, который при вставке продукта в 
+таблицу products, будет заносить имя, цену и текущую 
+дату в таблицу history_of_price.
+*/
+create table history_of_price (
+    id serial primary key,
+    name varchar(50),
+    price integer,
+    date timestamp
+);
+
+create or replace function products_history()
+    returns trigger as
+$$
+    BEGIN
+        insert into history_of_price (name, price, date)
+        values(name = new.name, price = new.price, current_date);
+		where id = new.id;
+        return NEW;
+    END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger products_history_trigger
+    after insert
+    on products
+    for each row
+    execute procedure products_history();
